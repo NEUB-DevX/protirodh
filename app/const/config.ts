ea074672bff964f493eab23b8b27f08fec9c81a9
AHN <ahndo7059@gmail.com>
2025-11-08 00:07:53 +0600

    add

--- app/const/config.ts
+++ app/const/config.ts
@@ -1,2 +1,2 @@
-// export const API_URL="https://regina-untalented-sigmoidally.ngrok-free.dev/api/v1/"
-export const API_URL="http://localhost:4113/api/v1/"
+export const API_URL="https://regina-untalented-sigmoidally.ngrok-free.dev/api/v1/"
+// export const API_URL="http://localhost:4113/api/v1/"
